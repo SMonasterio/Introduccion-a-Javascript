f36bc42376b71cbc92a9baced8bb8e58fd6d9905
Sofia Monasterio <sofimonasterio93@gmail.com>
2021-05-03 15:45:32 -0300

    introducioón javaScript

---
+++ app.js
@@ -0,0 +1,63 @@
+//  Variable:
+// Espacio de memoria donde guardo datos
+
+var nombre = "Eugenio"  
+let apellido = "Perez"
+
+const pi=3.14
+
+// Tipos de datos
+
+//String
+let cliente ="Fabrizio"
+
+//Number
+let numero1 =34
+let numero2 =67.25
+let Numero3 =-24
+
+//Boolean
+let comprar =true
+let comer =false
+
+//Null
+let billetera =null
+
+//Undefined
+let caja
+
+//Array
+let areglo =["Pablo, 689, true"]
+
+//Object
+let persona = {
+    nombre ="Jose",
+    apellido ="Mandarino",
+    edad =33,
+}
+
+//Operadores
+
+//Aritméticos
+let suma =numero1 + numero2
+let resta =numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+let resto = numero1 % numero2;
+
+//Untiarios
+++numero1;
+--numero1;
+
+//Relacionales 
+console.log(numero1 == numero2);
+console.log(numero1 >= numero2);
+console.log(numero1<= numero2);
+console.log(numero1 != numero2);
+ 
+console.log ("Hola mundo");
+
+//Negación
+let verdadero =true;
+console.log(!verdadero);
+
